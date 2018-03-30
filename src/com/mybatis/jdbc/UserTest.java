45084c605ff27f81bbc9ad84e0142cf6a7e18c3a
qiang <qiang@172.24.39.241>
2018-03-30 21:34:21 +0800

    搭建，测试，findById,findByName

---
+++ src/com/mybatis/jdbc/UserTest.java
@@ -0,0 +1,69 @@
+package com.mybatis.jdbc;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.management.openmbean.OpenMBeanAttributeInfoSupport;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import com.test.pojo.User;
+
+public class UserTest {
+
+	@Test
+	public void findById()   {
+		
+		String resource = "SqlMapConfig.xml";
+		InputStream inputStream = null;
+		SqlSession openSession = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+			 openSession = factory.openSession();
+			User user = openSession.selectOne("test.findById", 1);
+			System.out.println(user);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			openSession.close();
+		}
+		
+	}
+	
+	@Test
+	public void findByName() {
+		
+		String resource = "SqlMapConfig.xml";
+		InputStream inputStream = null;
+		SqlSession openSession = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+			
+		     openSession = factory.openSession();
+			List<User> list = openSession.selectList("test.findByName", "%王%");
+			System.out.println(list);
+		} catch (IOException e) {
+			e.printStackTrace();
+			
+		}finally {
+			openSession.close();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
